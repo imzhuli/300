74bf8b013de201ae9af28fe7baa677f6dbb0d537
zhuli <imzhuli@vip.qq.com>
2023-07-17 17:24:59 +0800

    remove old version before config & make

--- scripts/sub_build/001_CppSample.sh
+++ scripts/sub_build/001_CppSample.sh
@@ -7,8 +7,15 @@ if [ "$BUILD_ENV_CHECKED" != true ] ; then
     exit 1
 fi
 
-CMAKE_FLAGS="--no-warn-unused-cli -Wno-dev -DX_OPTION_STATIC=true"
+# remove old install
+rm -rf "$CMAKE_INSTALL_PREFIX"/include/glm
+rm -rf "$CMAKE_INSTALL_PREFIX"/include/rapidjson
+rm -rf "$CMAKE_INSTALL_PREFIX"/include/xel
+rm -rf "$CMAKE_INSTALL_PREFIX"/include/xel_ext
+rm -rf "$CMAKE_INSTALL_PREFIX"/lib/libxel.a
 
+# build
+CMAKE_FLAGS="--no-warn-unused-cli -Wno-dev -DX_OPTION_STATIC=true"
 CPPSAMPLE_BUILD_DIR="$CPPSAMPLE_DIR/build"
 rm -rf "$CPPSAMPLE_BUILD_DIR"
 mkdir -p "${CPPSAMPLE_BUILD_DIR}"
@@ -16,13 +23,6 @@ cd "${CPPSAMPLE_BUILD_DIR}"
 
 cmake -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_PREFIX" ${CMAKE_FLAGS} "$CPPSAMPLE_SOURCE_DIR"
 make -j --
-
-# remove old install
-rm -rf "$CMAKE_INSTALL_PREFIX"/include/glm
-rm -rf "$CMAKE_INSTALL_PREFIX"/include/rapidjson
-rm -rf "$CMAKE_INSTALL_PREFIX"/include/xel
-rm -rf "$CMAKE_INSTALL_PREFIX"/include/xel_ext
-rm -rf "$CMAKE_INSTALL_PREFIX"/lib/libxel.a
 
 # new installation
 make install
--- scripts/sub_build/100_Assets.sh
+++ scripts/sub_build/100_Assets.sh
@@ -7,8 +7,11 @@ if [ "$BUILD_ENV_CHECKED" != true ] ; then
     exit 1
 fi
 
+# setup & remove old install
 ASSETS_INSTALL_DIR="$P300_INSTALL_DIR/Assets"
+rm -rf "$ASSETS_INSTALL_DIR"
 
+# copy fonts
 ASSETS_FONT_INSTALL_DIR="$ASSETS_INSTALL_DIR/fonts"
 mkdir -p "$ASSETS_FONT_INSTALL_DIR"
 cp "$ASSETS_FONT_SOURCE_DIR/"*.woff2 "$ASSETS_FONT_INSTALL_DIR/"
